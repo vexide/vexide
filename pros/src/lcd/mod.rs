3899a055f3a1373207069af06bb1101e8ce4c948
Gavin Niederman <gavinniederman@gmail.com>
2023-05-09 20:38:06 -0700

    fix: initialize lcd when creating writer.

--- pros/src/lcd/mod.rs
+++ pros/src/lcd/mod.rs
@@ -5,5 +5,8 @@ pub mod buttons;
 pub(crate) mod writer;
 
 lazy_static::lazy_static! {
-    pub(crate) static ref WRITER: spin::Mutex<writer::Writer> = spin::Mutex::new(writer::Writer::new());
+    pub(crate) static ref WRITER: spin::Mutex<writer::Writer> = {
+        unsafe { pros_sys::lcd_initialize() };
+        spin::Mutex::new(writer::Writer::new())
+    };
 }
