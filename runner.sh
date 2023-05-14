74a4106f551c76559ba02c74f535415ba9ee36f9
Gavin Niederman <gavinniederman@gmail.com>
2023-05-13 19:40:09 -0700

    add: better runner

--- runner.sh
+++ runner.sh
@@ -1,3 +1,7 @@
-cargo build -p example --release;
-arm-none-eabi-objcopy --strip-symbol=install_hot_table --strip-symbol=__libc_init_array --strip-symbol=_PROS_COMPILE_DIRECTORY --strip-symbol=_PROS_COMPILE_TIMESTAMP --strip-symbol=_PROS_COMPILE_TIMESTAMP_INT target/armv7a-vexos/release/example target/armv7a-vexos/release/example.stripped;
-arm-none-eabi-objcopy -O binary -R .hot_init target/armv7a-vexos/release/example.stripped  target/release/out.bin;
+echo "stripping..."
+arm-none-eabi-objcopy --strip-symbol=install_hot_table --strip-symbol=__libc_init_array --strip-symbol=_PROS_COMPILE_DIRECTORY --strip-symbol=_PROS_COMPILE_TIMESTAMP --strip-symbol=_PROS_COMPILE_TIMESTAMP_INT $1 $1.stripped;
+echo "[ok]"
+echo "converting elf to bin..."
+arm-none-eabi-objcopy -O binary -R .hot_init $1.stripped  $1.bin;
+echo "[ok]"
+echo "output at $1.bin"
