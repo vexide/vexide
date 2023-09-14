c314c57a8a57e846264b66b73c29fa078c0ffa6c
Gavin-Niederman <gavinniederman@gmail.com>
2023-09-14 14:20:28 -0700

    move functions to crate

--- pros-sys/src/lib.rs
+++ pros-sys/src/lib.rs
@@ -4,8 +4,13 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
+const CLOCKS_PER_SEC: u32 = 1000;
+
+
 extern "C" {
     pub fn memalign(alignment: usize, size: usize) -> *mut core::ffi::c_void;
+    pub fn __errno() -> *mut i32;
+    pub fn clock() -> i32;
 }
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
