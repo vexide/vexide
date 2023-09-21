f7891011719df706832163160c72e2ddadd88d05
Gavin-Niederman <gavinniederman@gmail.com>
2023-09-20 20:07:57 -0700

    bundle pros with pros-sys

--- pros-sys/build.rs
+++ pros-sys/build.rs
@@ -1,28 +1,5 @@
-use std::env;
-
 fn main() {
-    if let Some(docs_rs) = env::var_os("DOCS_RS") {
-        if docs_rs == "1" {
-            return;
-        }
-    }
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-
-    let pros_bytes = reqwest::blocking::get(
-        "https://github.com/purduesigbots/pros/releases/download/3.8.0/kernel@3.8.0.zip",
-    )
-    .unwrap()
-    .bytes()
-    .unwrap();
-    let pros_bytes: Vec<_> = pros_bytes.into_iter().collect();
-
-    std::fs::write(format!("{out_dir}/pros.zip"), pros_bytes).unwrap();
-
-    zip::ZipArchive::new(std::fs::File::open(format!("{out_dir}/pros.zip")).unwrap())
-        .unwrap()
-        .extract(&out_dir)
-        .unwrap();
-
-    println!("cargo:rustc-link-search=native={out_dir}/firmware");
+    println!("cargo:rustc-link-search=native={manifest_dir}/link");
 }
