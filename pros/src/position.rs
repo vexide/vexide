3ebffb4002eaf25aea20b4dc7630f0aae19a2bca
doinkythederp <doinkythederp@icloud.com>
2023-09-29 21:50:48 +0000

    feat(Position): add a worrying amount of ops

--- pros/src/position.rs
+++ pros/src/position.rs
@@ -1,3 +1,5 @@
+use core::{cmp::Ordering, ops::*};
+
 //TODO: Add more unit types to this.
 /// Represents a position a motor can travel to.
 /// Positions are relative to the last position the motor was zeroed to.
@@ -50,5 +52,95 @@ impl Position {
             Self::Rotations(num) => (num * 4096.0) as i64,
             Self::Counts(num) => num,
         }
+    }
+}
+
+impl Add for Position {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::from_degrees(self.into_degrees() + rhs.into_degrees())
+    }
+}
+
+impl AddAssign for Position {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub for Position {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::from_degrees(self.into_degrees() - rhs.into_degrees())
+    }
+}
+
+impl SubAssign for Position {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl Mul<Self> for Position {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::from_degrees(self.into_degrees() * rhs.into_degrees())
+    }
+}
+
+impl MulAssign<Self> for Position {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = *self * rhs;
+    }
+}
+
+impl Div<Self> for Position {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self::from_degrees(self.into_degrees() / rhs.into_degrees())
+    }
+}
+
+impl DivAssign<Self> for Position {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = *self / rhs;
+    }
+}
+
+impl Rem<Self> for Position {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self::from_degrees(self.into_degrees() % rhs.into_degrees())
+    }
+}
+
+impl RemAssign<Self> for Position {
+    fn rem_assign(&mut self, rhs: Self) {
+        *self = *self % rhs;
+    }
+}
+
+impl Neg for Position {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self::from_degrees(-self.into_degrees())
+    }
+}
+
+impl PartialEq for Position {
+    fn eq(&self, other: &Self) -> bool {
+        self.into_degrees() == other.into_degrees()
+    }
+}
+
+impl PartialOrd for Position {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.into_degrees().partial_cmp(&other.into_degrees())
     }
 }
