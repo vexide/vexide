68da3a11acb8dee0b114edd45477e5b224e637fa
doinkythederp <doinkythederp@icloud.com>
2023-10-06 19:43:13 -0700

    feat(sys): allow not linking for off-hardware uses

--- pros-sys/build.rs
+++ pros-sys/build.rs
@@ -1,5 +1,12 @@
-fn main() {
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+use cfg_if::cfg_if;
 
-    println!("cargo:rustc-link-search=native={manifest_dir}/link");
+fn main() {
+    cfg_if! {
+        if #[cfg(not(feature = "no-link"))] {
+            let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+            #[cfg(not(feature = "no-link"))]
+            println!("cargo:rustc-link-search=native={manifest_dir}/link");
+        }
+    }
 }
