95ab3f814857e28c7d8748ea6e744c599e48971c
Gavin-Niederman <gavinniederman@gmail.com>
2024-01-01 12:11:29 -0800

    fix: update compiling instructions and flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,16 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    cargo-pros.url = "github:pros-rs/cargo-pros";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: (flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          openssl
-          pkgconfig
-          clang
-          libclang
-          gcc-arm-embedded-9
-        ];
-
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-      };
-    }
-  ));
+  outputs = { nixpkgs, flake-utils, cargo-pros, ... }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cargo-pros' = cargo-pros.packages.${system}.default;
+      in {
+        devShell =
+          pkgs.mkShell { buildInputs = with pkgs; [ gcc-arm-embedded-9 cargo-pros' ]; };
+      }));
 }
