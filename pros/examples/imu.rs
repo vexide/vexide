2ba4141c3344ed42cc43df14c725d522609fae86
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-04 18:06:13 -0600

    fix: revert test code in example

--- pros/examples/imu.rs
+++ pros/examples/imu.rs
@@ -14,34 +14,9 @@ impl SyncRobot for Robot {
         imu.calibrate()?;
 
         loop {
-            // let euler = imu.euler()?;
+            let euler = imu.euler()?;
 
-            // println!("Pitch: {} Roll: {} Yaw: {}", euler.pitch, euler.roll, euler.yaw);
-
-            let delay = Duration::from_secs(1);
-
-            println!("{:?}", imu.is_calibrating());
-            pros::task::delay(delay);
-            println!("{:?}", imu.rotation());
-            pros::task::delay(delay);
-            println!("{:?}", imu.heading());
-            pros::task::delay(delay);
-            println!("{:?}", imu.pitch());
-            pros::task::delay(delay);
-            println!("{:?}", imu.roll());
-            pros::task::delay(delay);
-            println!("{:?}", imu.yaw());
-            pros::task::delay(delay);
-            println!("{:?}", imu.status());
-            pros::task::delay(delay);
-            println!("{:?}", imu.quaternion());
-            pros::task::delay(delay);
-            println!("{:?}", imu.euler());
-            pros::task::delay(delay);
-            println!("{:?}", imu.gyro_rate());
-            pros::task::delay(delay);
-            println!("{:?}", imu.accel());
-            pros::task::delay(delay);
+            println!("Pitch: {} Roll: {} Yaw: {}", euler.pitch, euler.roll, euler.yaw);
 
             pros::task::delay(Duration::from_secs(1));
         }
