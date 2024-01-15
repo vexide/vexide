f0d5d78c7d9227ef1f5e34581ff1654fcb4ed961
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-14 23:43:36 -0600

    chore: fmt ⸨◺_◿⸩

--- pros/src/competition.rs
+++ pros/src/competition.rs
@@ -9,29 +9,29 @@ const COMPETITION_SYSTEM: u8 = 1 << 3;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CompetitionMode {
     /// The Disabled competition mode.
-    /// 
+    ///
     /// When in disabled mode, motor voltage commands to motors are disabled. Motors are
     /// forcibly locked to the "coast" brake mode.
-    /// 
+    ///
     /// Robots may be placed into disabled mode at any point in the competition after
     /// connecting, but are typically disabled before the autonomous period, between
     /// autonomous and opcontrol periods, and following the opcontrol period of a match.
     Disabled,
-    
+
     /// The Autonomous competition mode.
-    /// 
+    ///
     /// When in autonomous mode, all motors and sensors may be controlled, however user
     /// input from controller buttons and joysticks is not available to be read.
-    /// 
+    ///
     /// Robots may be placed into autonomous mode at any point in the competition after
     /// connecting, but are typically placed into this mode at the start of a match.
     Autonomous,
 
     /// The Opcontrol competition mode.
-    /// 
+    ///
     /// When in opcontrol mode, all device access is available including access to
     /// controller joystick values for reading user-input from drive team members.
-    /// 
+    ///
     /// Robots may be placed into opcontrol mode at any point in the competition after
     /// connecting, but are typically placed into this mode following the autonomous
     /// period.
