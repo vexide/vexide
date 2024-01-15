7075dcc49941c365f1c0928067b224d0fd149035
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-14 22:50:39 -0600

    refactor: competition API

--- pros/src/competition.rs
+++ pros/src/competition.rs
@@ -1,71 +1,51 @@
 //! Utilities for getting what state of the competition the robot is in.
 //!
-//! You have the option of getting the entire state ([`get_status`]), or checking a specific one ([`is_autonomous`], etc.).
-//! Once a [`CompetitionStatus`] is created by [`get_status`] it will not be updated again.
+use pros_sys::misc::{COMPETITION_AUTONOMOUS, COMPETITION_CONNECTED, COMPETITION_DISABLED};
 
-/// The current status of the robot, allowing checks to be made
-/// for autonomous, disabled, and connected states.
+// TODO: change this to use PROS' internal version once we switch to PROS 4.
+const COMPETITION_SYSTEM: u8 = 1 << 3;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct CompetitionStatus(pub u8);
+pub enum CompetitionMode {
+    Disabled,
+    Opcontrol,
+    Autonomous,
+}
 
-/// Represents a type of system used to control competition state.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CompetitionSystem {
-    /// Competition state is controlled by a VEX Field Controller.
     FieldControl,
-
-    // Competition state is controlled by a VEX competition switch.
     CompetitionSwitch,
 }
 
-// TODO: Change this to use competition_is_field and competition_is_switch once we support PROS 4
-const COMPETITION_SYSTEM: u8 = 1 << 3;
+pub fn mode() -> CompetitionMode {
+    let status = unsafe { pros_sys::misc::competition_get_status() };
 
-impl CompetitionStatus {
-    pub const fn autonomous(&self) -> bool {
-        self.0 & pros_sys::misc::COMPETITION_AUTONOMOUS != 0
+    if status & COMPETITION_DISABLED != 0 {
+        CompetitionMode::Disabled
+    } else if status & COMPETITION_AUTONOMOUS != 0 {
+        CompetitionMode::Autonomous
+    } else {
+        CompetitionMode::Opcontrol
     }
-    pub const fn disabled(&self) -> bool {
-        self.0 & pros_sys::misc::COMPETITION_DISABLED != 0
-    }
-    pub const fn connected(&self) -> bool {
-        self.0 & pros_sys::misc::COMPETITION_CONNECTED != 0
-    }
-    pub fn system(&self) -> Option<CompetitionSystem> {
-        if self.connected() {
-            if self.0 & COMPETITION_SYSTEM == 0 {
-                Some(CompetitionSystem::FieldControl)
-            } else {
-                Some(CompetitionSystem::CompetitionSwitch)
-            }
+}
+
+pub fn connected() -> bool {
+    let status = unsafe { pros_sys::misc::competition_get_status() };
+
+    status & COMPETITION_CONNECTED != 0
+}
+
+pub fn system() -> Option<CompetitionSystem> {
+    let status = unsafe { pros_sys::misc::competition_get_status() };
+
+    if status & COMPETITION_CONNECTED != 0 {
+        if status & COMPETITION_SYSTEM == 0 {
+            Some(CompetitionSystem::FieldControl)
         } else {
-            None
+            Some(CompetitionSystem::CompetitionSwitch)
         }
+    } else {
+        None
     }
-}
-
-/// Get the current status of the robot.
-pub fn get_status() -> CompetitionStatus {
-    CompetitionStatus(unsafe { pros_sys::misc::competition_get_status() })
-}
-
-/// Get the type of system currently controlling the robot's competition state, or none if the robot
-/// is not tethered to a competition controller.
-pub fn get_system() -> Option<CompetitionSystem> {
-    get_status().system()
-}
-
-/// Check if the robot is in autonomous mode.
-pub fn is_autonomous() -> bool {
-    unsafe { pros_sys::misc::competition_is_autonomous() }
-}
-
-/// Check if the robot is disabled.
-pub fn is_disabled() -> bool {
-    unsafe { pros_sys::misc::competition_is_disabled() }
-}
-
-/// Check if the robot is connected to a VEX field or competition switch.
-pub fn is_connected() -> bool {
-    unsafe { pros_sys::misc::competition_is_connected() }
 }
