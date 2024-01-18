e1606f73eceb945023e7240d805021f156a619a8
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-18 17:59:09 -0600

    refactor: match port naming with peripherals

--- packages/pros/src/devices/smart/expander.rs
+++ packages/pros/src/devices/smart/expander.rs
@@ -3,14 +3,14 @@ use crate::devices::adi::AdiPort;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct AdiExpander {
-    pub adi_port_a: AdiPort,
-    pub adi_port_b: AdiPort,
-    pub adi_port_c: AdiPort,
-    pub adi_port_d: AdiPort,
-    pub adi_port_e: AdiPort,
-    pub adi_port_f: AdiPort,
-    pub adi_port_g: AdiPort,
-    pub adi_port_h: AdiPort,
+    pub adi_1: AdiPort,
+    pub adi_2: AdiPort,
+    pub adi_3: AdiPort,
+    pub adi_4: AdiPort,
+    pub adi_5: AdiPort,
+    pub adi_6: AdiPort,
+    pub adi_7: AdiPort,
+    pub adi_8: AdiPort,
     port: SmartPort,
 }
 
@@ -19,14 +19,14 @@ impl AdiExpander {
     pub fn new(port: SmartPort) -> Self {
         unsafe {
             Self {
-                adi_port_a: AdiPort::new(1, Some(port.index())),
-                adi_port_b: AdiPort::new(2, Some(port.index())),
-                adi_port_c: AdiPort::new(3, Some(port.index())),
-                adi_port_d: AdiPort::new(4, Some(port.index())),
-                adi_port_e: AdiPort::new(5, Some(port.index())),
-                adi_port_f: AdiPort::new(6, Some(port.index())),
-                adi_port_g: AdiPort::new(7, Some(port.index())),
-                adi_port_h: AdiPort::new(8, Some(port.index())),
+                adi_1: AdiPort::new(1, Some(port.index())),
+                adi_2: AdiPort::new(2, Some(port.index())),
+                adi_3: AdiPort::new(3, Some(port.index())),
+                adi_4: AdiPort::new(4, Some(port.index())),
+                adi_5: AdiPort::new(5, Some(port.index())),
+                adi_6: AdiPort::new(6, Some(port.index())),
+                adi_7: AdiPort::new(7, Some(port.index())),
+                adi_8: AdiPort::new(8, Some(port.index())),
                 port,
             }
         }
