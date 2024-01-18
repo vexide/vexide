27ebaa711f4d650debef36b02535e65eb47ce212
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-17 23:13:51 -0600

    refactor: prefix adi ports with `adi_`

--- packages/pros/src/devices/smart/expander.rs
+++ packages/pros/src/devices/smart/expander.rs
@@ -3,14 +3,14 @@ use crate::devices::adi::AdiPort;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct AdiExpander {
-    pub port_a: AdiPort,
-    pub port_b: AdiPort,
-    pub port_c: AdiPort,
-    pub port_d: AdiPort,
-    pub port_e: AdiPort,
-    pub port_f: AdiPort,
-    pub port_g: AdiPort,
-    pub port_h: AdiPort,
+    pub adi_port_a: AdiPort,
+    pub adi_port_b: AdiPort,
+    pub adi_port_c: AdiPort,
+    pub adi_port_d: AdiPort,
+    pub adi_port_e: AdiPort,
+    pub adi_port_f: AdiPort,
+    pub adi_port_g: AdiPort,
+    pub adi_port_h: AdiPort,
     port: SmartPort,
 }
 
@@ -19,14 +19,14 @@ impl AdiExpander {
     pub fn new(port: SmartPort) -> Self {
         unsafe {
             Self {
-                port_a: AdiPort::new(1, Some(port.index())),
-                port_b: AdiPort::new(2, Some(port.index())),
-                port_c: AdiPort::new(3, Some(port.index())),
-                port_d: AdiPort::new(4, Some(port.index())),
-                port_e: AdiPort::new(5, Some(port.index())),
-                port_f: AdiPort::new(6, Some(port.index())),
-                port_g: AdiPort::new(7, Some(port.index())),
-                port_h: AdiPort::new(8, Some(port.index())),
+                adi_port_a: AdiPort::new(1, Some(port.index())),
+                adi_port_b: AdiPort::new(2, Some(port.index())),
+                adi_port_c: AdiPort::new(3, Some(port.index())),
+                adi_port_d: AdiPort::new(4, Some(port.index())),
+                adi_port_e: AdiPort::new(5, Some(port.index())),
+                adi_port_f: AdiPort::new(6, Some(port.index())),
+                adi_port_g: AdiPort::new(7, Some(port.index())),
+                adi_port_h: AdiPort::new(8, Some(port.index())),
                 port,
             }
         }
