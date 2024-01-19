bd3e964d6d2fe973d9b35d1f510610e5187c9dcc
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-18 18:03:30 -0600

    refactor: use letters rather than numbers to distinguish adi ports

--- packages/pros/src/devices/smart/expander.rs
+++ packages/pros/src/devices/smart/expander.rs
@@ -10,14 +10,14 @@ use crate::devices::adi::AdiPort;
 /// be partially moved out of this struct to create devices.
 #[derive(Debug, Eq, PartialEq)]
 pub struct AdiExpander {
-    pub adi_1: AdiPort,
-    pub adi_2: AdiPort,
-    pub adi_3: AdiPort,
-    pub adi_4: AdiPort,
-    pub adi_5: AdiPort,
-    pub adi_6: AdiPort,
-    pub adi_7: AdiPort,
-    pub adi_8: AdiPort,
+    pub adi_a: AdiPort,
+    pub adi_b: AdiPort,
+    pub adi_c: AdiPort,
+    pub adi_d: AdiPort,
+    pub adi_e: AdiPort,
+    pub adi_f: AdiPort,
+    pub adi_g: AdiPort,
+    pub adi_h: AdiPort,
     port: SmartPort,
 }
 
@@ -26,14 +26,14 @@ impl AdiExpander {
     pub fn new(port: SmartPort) -> Self {
         unsafe {
             Self {
-                adi_1: AdiPort::new(1, Some(port.index())),
-                adi_2: AdiPort::new(2, Some(port.index())),
-                adi_3: AdiPort::new(3, Some(port.index())),
-                adi_4: AdiPort::new(4, Some(port.index())),
-                adi_5: AdiPort::new(5, Some(port.index())),
-                adi_6: AdiPort::new(6, Some(port.index())),
-                adi_7: AdiPort::new(7, Some(port.index())),
-                adi_8: AdiPort::new(8, Some(port.index())),
+                adi_a: AdiPort::new(1, Some(port.index())),
+                adi_b: AdiPort::new(2, Some(port.index())),
+                adi_c: AdiPort::new(3, Some(port.index())),
+                adi_d: AdiPort::new(4, Some(port.index())),
+                adi_e: AdiPort::new(5, Some(port.index())),
+                adi_f: AdiPort::new(6, Some(port.index())),
+                adi_g: AdiPort::new(7, Some(port.index())),
+                adi_h: AdiPort::new(8, Some(port.index())),
                 port,
             }
         }
