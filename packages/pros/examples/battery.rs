fade7018b34ee3448e942f1f4bd50abf394cc55c
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-18 21:25:19 -0600

    chore: fmt 🤬🤬

--- packages/pros/examples/battery.rs
+++ packages/pros/examples/battery.rs
@@ -1,19 +1,18 @@
 #![no_std]
 #![no_main]
 
-use pros::prelude::*;
-use pros::devices::battery;
+use pros::{devices::battery, prelude::*};
 
 #[derive(Default)]
 pub struct Robot;
 
 impl AsyncRobot for Robot {
     async fn opcontrol(&mut self) -> pros::Result {
-		if battery::capacity()? < 20.0 {
-			println!("Battery is low!");
-		} else if battery::temperature()? > 999.0 {
-			println!("Battery has exploded!");
-		}
+        if battery::capacity()? < 20.0 {
+            println!("Battery is low!");
+        } else if battery::temperature()? > 999.0 {
+            println!("Battery has exploded!");
+        }
 
         Ok(())
     }
