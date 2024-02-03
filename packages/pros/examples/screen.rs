f94dafd151d17067507fb37fd323e16b4ebda803
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-02-03 00:40:55 -0600

    docs: update examples

---
+++ packages/pros/examples/screen.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+use pros::prelude::*;
+use core::fmt::Write;
+
+pub struct Robot {
+    screen: Screen,
+}
+
+impl Robot {
+    fn new(peripherals: Peripherals) -> Self {
+        Self {
+            screen: peripherals.screen,
+        }
+    }
+}
+
+impl AsyncRobot for Robot {
+    async fn opcontrol(&mut self) -> pros::Result {
+        self.screen.fill(&Rect::new(0, 0, 20, 20), Rgb::RED)?;
+        self.screen.stroke(&Circle::new(25, 25, 20), Rgb::BLUE)?;
+
+        writeln!(self.screen, "Hello, world.")?;
+
+        Ok(())
+    }
+}
+async_robot!(Robot, Robot::new(Peripherals::take().unwrap()));
