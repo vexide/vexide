bfa72ca7344c68a502b7f03fc4f7a2b1d6e6e97f
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-03-24 11:33:27 -0500

    refactor: `usd_installed`

--- packages/vex-devices/src/usd.rs
+++ packages/vex-devices/src/usd.rs
@@ -2,7 +2,9 @@
 //!
 //! The USD API provides functions for interacting with the SD card slot on the V5 Brain.
 
+use vex_sys::vexFileDriveStatus;
+
 /// Checks if an SD card is installed.
 pub fn usd_installed() -> bool {
-    unsafe { pros_sys::misc::usd_is_installed() == 1 }
+    unsafe { vexFileDriveStatus(0) }
 }
