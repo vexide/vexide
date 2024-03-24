c3f119b7b8988136164605081f2d6c98eb63f2f7
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-03-24 11:11:29 -0500

    refactor: battery API

--- packages/pros-devices/src/battery.rs
+++ packages/pros-devices/src/battery.rs
@@ -1,46 +1,29 @@
 //! Utilites for getting information about the robot's battery.
 
-use pros_core::{bail_on, map_errno};
-use pros_sys::{PROS_ERR, PROS_ERR_F};
-use snafu::Snafu;
+use vex_sys::{
+    vexBatteryCapacityGet, vexBatteryCurrentGet, vexBatteryTemperatureGet, vexBatteryVoltageGet,
+};
 
 /// Get the robot's battery capacity.
-pub fn capacity() -> Result<f64, BatteryError> {
-    Ok(bail_on!(PROS_ERR_F, unsafe {
-        pros_sys::misc::battery_get_capacity()
-    }))
+/// TODO: Determine units
+pub fn capacity() -> f64 {
+    unsafe { vexBatteryCapacityGet() }
 }
 
 /// Get the current temperature of the robot's battery.
-pub fn temperature() -> Result<f64, BatteryError> {
-    Ok(bail_on!(PROS_ERR_F, unsafe {
-        pros_sys::misc::battery_get_temperature()
-    }))
+/// TODO: Determine units
+pub fn temperature() -> f64 {
+    unsafe { vexBatteryTemperatureGet() }
 }
 
 /// Get the electric current of the robot's battery.
-pub fn current() -> Result<i32, BatteryError> {
-    Ok(bail_on!(PROS_ERR, unsafe {
-        pros_sys::misc::battery_get_current()
-    }))
+/// TODO: Determine units
+pub fn current() -> i32 {
+    unsafe { vexBatteryCurrentGet() }
 }
 
 /// Get the robot's battery voltage.
-pub fn voltage() -> Result<i32, BatteryError> {
-    Ok(bail_on!(PROS_ERR, unsafe {
-        pros_sys::misc::battery_get_voltage()
-    }))
-}
-
-#[derive(Debug, Snafu)]
-/// Errors that can occur when interacting with the robot's battery.
-pub enum BatteryError {
-    /// Another resource is already using the battery.
-    ConcurrentAccess,
-}
-
-map_errno! {
-    BatteryError {
-        EACCES => Self::ConcurrentAccess,
-    }
+/// TODO: Determine units
+pub fn voltage() -> i32 {
+    unsafe { vexBatteryVoltageGet() }
 }
