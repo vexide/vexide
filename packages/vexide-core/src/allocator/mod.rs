b936d7db8f92a3eb14bb38295873debf30831318
Gavin-Niederman <gavinniederman@gmail.com>
2024-04-04 19:14:34 -0700

    fix: set target_os to none

--- packages/vexide-core/src/allocator/mod.rs
+++ packages/vexide-core/src/allocator/mod.rs
@@ -1,6 +1,6 @@
 //! Simple allocator using the VEX libc allocation functions in vexos and jemalloc in the sim.
 
-#[cfg(target_os = "vexos")]
+#[cfg(all(target_arch = "arm", target_os = "none"))]
 pub mod vexos;
 #[cfg(target_arch = "wasm32")]
 mod wasm;
