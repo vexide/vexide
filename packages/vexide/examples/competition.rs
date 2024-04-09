29068b0258ef56888931336099baf8c9b33684de
Max Niederman <max@maxniederman.com>
2024-04-08 21:47:39 -0700

    docs(competition): add competition builder example

--- packages/vexide/examples/competition.rs
+++ packages/vexide/examples/competition.rs
@@ -4,38 +4,43 @@
 
 extern crate alloc;
 
-use core::time::Duration;
+use alloc::boxed::Box;
 
 use vexide::prelude::*;
 
-struct Robot;
-
-impl CompetitionRobot for Robot {
-    type Error = !;
-
-    async fn connected(&mut self) -> Result<(), !> {
-        println!("Connected");
-        Ok(())
-    }
-    async fn disconnected(&mut self) -> Result<(), !> {
-        println!("Disconnected");
-        Ok(())
-    }
-    async fn disabled(&mut self) -> Result<(), !> {
-        println!("Disabled");
-        Ok(())
-    }
-    async fn driver(&mut self) -> Result<(), !> {
-        println!("Driver");
-        Ok(())
-    }
-    async fn autonomous(&mut self) -> Result<(), !> {
-        println!("Autonomous");
-        Ok(())
-    }
-}
-
 #[vexide_startup::main]
-async fn main(peripherals: Peripherals) {
-    Robot.compete().await;
+async fn main(_peripherals: Peripherals) {
+    Competition::builder(())
+        .on_connected(|_| {
+            Box::pin(async {
+                println!("Connected");
+                Ok::<_, !>(())
+            })
+        })
+        .on_disconnected(|_| {
+            Box::pin(async {
+                println!("Disconnected");
+                Ok(())
+            })
+        })
+        .while_disabled(|_| {
+            Box::pin(async {
+                println!("Disabled");
+                Ok(())
+            })
+        })
+        .while_driver_controlled(|_| {
+            Box::pin(async {
+                println!("Driver");
+                Ok(())
+            })
+        })
+        .while_in_autonomous(|_| {
+            Box::pin(async {
+                println!("Autonomous");
+                Ok(())
+            })
+        })
+        .await
+        .unwrap();
 }
