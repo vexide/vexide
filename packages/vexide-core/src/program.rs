45d306879b8578b044e734828e9e990224785fbf
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-04-12 11:06:12 -0500

    refactor: poll vexSerialWriteFree rather than a hardcoded polling time

---
+++ packages/vexide-core/src/program.rs
@@ -0,0 +1,24 @@
+pub use vex_sdk::{vexSystemExitRequest, vexTasksRun, vexSerialWriteFree};
+
+pub use crate::time::Instant;
+pub use crate::io;
+
+/// Exits the program using vexSystemExitRequest.
+/// This function will not instantly exit the program,
+/// but will instead wait 3ms to force the serial buffer to flush.
+pub fn exit() -> ! {
+    unsafe {
+        // Force the serial buffer to flush
+        while vexSerialWriteFree(io::STDIO_CHANNEL) != (io::Stdout::INTERNAL_BUFFER_SIZE as i32) {
+            vexTasksRun();
+        }
+        // Exit the program
+        // Everything after this point is unreachable.
+        vexSystemExitRequest();
+    }
+
+    // unreachable.
+    loop {
+        core::hint::spin_loop();
+    }
+}
