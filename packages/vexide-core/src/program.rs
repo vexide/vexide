b03cf4b38a3cbaaf3aef3ec6bac34e8606c12121
Gavin-Niederman <gavinniederman@gmail.com>
2024-04-12 14:22:41 -0700

    fix: add a timeout to serial flushing in case it cannot be flushed sucessfully

--- packages/vexide-core/src/program.rs
+++ packages/vexide-core/src/program.rs
@@ -3,6 +3,8 @@
 //! This module contains functions for accessing/modifying the state of the current
 //! user program.
 
+use core::time::Duration;
+
 pub use vex_sdk::{vexSystemExitRequest, vexTasksRun, vexSerialWriteFree};
 
 pub use crate::time::Instant;
@@ -12,9 +14,15 @@ pub use crate::io;
 /// This function will not instantly exit the program,
 /// but will instead wait 3ms to force the serial buffer to flush.
 pub fn exit() -> ! {
+    let exit_time = Instant::now();
+    const FLUSH_TIMEOUT: Duration = Duration::from_millis(15);
     unsafe {
         // Force the serial buffer to flush
-        while vexSerialWriteFree(io::STDIO_CHANNEL) != (io::Stdout::INTERNAL_BUFFER_SIZE as i32) {
+        while exit_time.elapsed() < FLUSH_TIMEOUT {
+            // If the buffer has been fully flushed, exit the loop
+            if vexSerialWriteFree(io::STDIO_CHANNEL) == (io::Stdout::INTERNAL_BUFFER_SIZE as i32) {
+                break;
+            }
             vexTasksRun();
         }
         // Exit the program
