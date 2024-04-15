efabce36774e49088bd2ad4a89a730e04a49a78c
Gavin-Niederman <gavinniederman@gmail.com>
2024-04-15 12:40:03 -0700

    docs: include usage info in graphics crate docs

--- packages/vexide-graphics/src/lib.rs
+++ packages/vexide-graphics/src/lib.rs
@@ -4,6 +4,17 @@
 //! # Features
 //! - `embedded-graphics`: Enables support for the [`embedded-graphics`](https://crates.io/crates/embedded-graphics) crate.
 //! - `slint`: Enables support for the Slint graphics library.
+//!
+//! # Usage
+//!
+//! ### Slint
+//! To use Slint, call [`initialize_slint_platform`](slint::initialize_slint_platform) and then continue using Slint as normal.
+//! If you get errors when building your slint files, try enabling the `EmbedResourcesKind` slint compile option.
+//!
+//! ### Embedded-graphics
+//! To use embedded-graphics, create a new [`BrainDisplay`](embedded_graphics::BrainDisplay) using its [`new`](embedded_graphics::BrainDisplay::new) function
+//! and start using it as a draw target.
+
 
 #![no_std]
 #![cfg_attr(feature = "embedded-graphics", feature(never_type))]
