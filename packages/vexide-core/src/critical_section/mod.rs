72c09529ceb3f37766e0eb749237981b2136635d
doinkythederp <doinkythederp@icloud.com>
2024-04-16 20:48:06 -0700

    docs: add critical section module overview

--- packages/vexide-core/src/critical_section/mod.rs
+++ packages/vexide-core/src/critical_section/mod.rs
@@ -1,3 +1,7 @@
+//! A protected section that cannot be entered by more than one "thread" of execution at a time.
+//!
+//! Provides implementations for the `critical_section` crate on various platforms.
+
 #[cfg(all(target_arch = "arm", target_os = "none"))]
 mod zynq;
 
