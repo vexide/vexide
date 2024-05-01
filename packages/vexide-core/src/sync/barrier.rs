0e6ded4c7bbddc397abd3a16d6165e33a46c4106
gniederman2083 <gniederman2083@student.pps.net>
2024-05-01 09:10:48 -0700

    docs: document `Barrier`

--- packages/vexide-core/src/sync/barrier.rs
+++ packages/vexide-core/src/sync/barrier.rs
@@ -2,6 +2,8 @@ use core::sync::atomic::AtomicUsize;
 
 use futures_core::Future;
 
+/// A future that resolves once all tasks have arrived at a [`Barrier`].
+/// This is created by [`Barrier::wait`].
 pub struct BarrierWaitFuture<'a> {
     leader: bool,
     barrier: &'a Barrier,
@@ -26,11 +28,34 @@ impl<'a> Future for BarrierWaitFuture<'a> {
     }
 }
 
+
+/// Allows for multiple tasks to reach the same point in execution before continuing.
+///
+/// # Examples
+///
+/// ```rust
+/// const N: usize = 10;
+/// let handles = Vec::new();
+/// let barrier = Barrier::new(N);
+/// for i in 0..N {
+///     spawn(async {
+///         // Every "Before barrier" will be printed before any "After Barrier".
+///         println!("Before Barrier");
+///         barrier.wait().await;
+///         println!("After Barrier");
+///     })
+/// }
+///
+/// for handle in handles {
+///     handle.await;
+/// }
+/// ```
 pub struct Barrier {
     count: usize,
     current: AtomicUsize,
 }
 impl Barrier {
+    /// Create a new barrier that will block `count` threads before releasing.
     pub const fn new(count: usize) -> Self {
         Self {
             count,
@@ -38,6 +63,11 @@ impl Barrier {
         }
     }
 
+    /// Wait for the barrier to be reached by every thread.
+    /// Currently [`Barrier`]s are not reusable.
+    ///
+    /// A single task will get a [`BarrierWaitFuture`] that resolves to true.
+    /// This is the equivalent of the standard library method [`BarrierWaitResult::is_leader`](https://doc.rust-lang.org/std/sync/struct.BarrierWaitResult.html#method.is_leader)
     pub fn wait(&self) -> BarrierWaitFuture<'_> {
         let leader = critical_section::with(|_| {
             self.current
