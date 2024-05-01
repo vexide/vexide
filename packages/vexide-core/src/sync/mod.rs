8600f077b9b5a4c66fc5ebb985217b9eea13eba1
Gavin-Niederman <gavinniederman@gmail.com>
2024-05-01 13:45:12 -0700

    chore: fmt

--- packages/vexide-core/src/sync/mod.rs
+++ packages/vexide-core/src/sync/mod.rs
@@ -3,13 +3,13 @@
 //! Types implemented here are specifically designed to mimic the standard library.
 
 mod barrier;
+mod condvar;
 mod mutex;
 mod once;
 mod rwlock;
-mod condvar;
 
 pub use barrier::{Barrier, BarrierWaitFuture};
+pub use condvar::{CondVar, CondVarWaitFuture};
 pub use mutex::{Mutex, MutexGuard, MutexLockFuture, RawMutex};
 pub use once::{Once, OnceLock};
 pub use rwlock::{RwLock, RwLockReadFuture, RwLockReadGuard, RwLockWriteFuture, RwLockWriteGuard};
-pub use condvar::{CondVar, CondVarWaitFuture};
