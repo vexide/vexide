69a8bcb663070eab6b5ca695e68cf196e7851633
Gavin-Niederman <gavinniederman@gmail.com>
2024-05-02 10:26:26 -0700

    fix: implement debug for `LazyLock`

--- packages/vexide-core/src/sync/lazy.rs
+++ packages/vexide-core/src/sync/lazy.rs
@@ -1,4 +1,4 @@
-use core::{cell::UnsafeCell, mem::MaybeUninit, ops::Deref};
+use core::{cell::UnsafeCell, fmt::Debug, mem::MaybeUninit, ops::Deref};
 
 use super::Once;
 
@@ -6,12 +6,14 @@ struct Data<T, I> {
     data: MaybeUninit<T>,
     init: MaybeUninit<I>,
 }
+/// A thread safe lazy initialized value.
 pub struct LazyLock<T, I = fn() -> T> {
     data: UnsafeCell<Data<T, I>>,
     once: Once,
 }
 unsafe impl<T: Send + Sync, I: Send> Sync for LazyLock<T, I> {}
 impl<T, I: FnOnce() -> T> LazyLock<T, I> {
+    /// Creates a new [`LazyLock`] with the given initializer.
     pub const fn new(init: I) -> Self {
         Self {
             data: UnsafeCell::new(Data {
@@ -22,15 +24,7 @@ impl<T, I: FnOnce() -> T> LazyLock<T, I> {
         }
     }
 
-    fn lazy_init(&self) {
-        let data = unsafe { &mut *self.data.get() };
-        let init = unsafe { data.init.assume_init_read() };
-        data.data = MaybeUninit::new((init)());
-        unsafe {
-            data.init.assume_init_drop();
-        }
-    }
-
+    /// Consume the [`LazyLock`] and return the inner value if it has been initialized.
     pub fn into_inner(self) -> Result<T, I> {
         let data = self.data.into_inner();
         match self.once.is_complete() {
@@ -39,16 +33,27 @@ impl<T, I: FnOnce() -> T> LazyLock<T, I> {
         }
     }
 
+    /// # Safety
+    /// Caller must ensure this function is only called once.
+    unsafe fn lazy_init(&self) {
+        let data = unsafe { &mut *self.data.get() };
+        let init = unsafe { data.init.assume_init_read() };
+        data.data = MaybeUninit::new((init)());
+        unsafe {
+            data.init.assume_init_drop();
+        }
+    }
+
     /// The equivalent of the standard libraries [`LazyLock::force`](https://doc.rust-lang.org/std/sync/struct.LazyLock.html#method.force).
     /// It has been renamed to get because it is the only way to asynchronously get the value.
     pub async fn get(&self) -> &T {
-        self.once.call_once(|| self.lazy_init()).await;
+        self.once.call_once(|| unsafe { self.lazy_init() }).await;
         unsafe { &(*self.data.get()).data.assume_init_ref() }
     }
 
-    fn get_blocking(&self) -> &T {
-        self.once.call_once_blocking(|| self.lazy_init());
-        unsafe { &(*self.data.get()).data.assume_init_ref() }
+    fn force(&self) -> &T {
+        self.once.call_once_blocking(|| unsafe { self.lazy_init() });
+        unsafe { (*self.data.get()).data.assume_init_ref() }
     }
 }
 impl<T, I: Default> Default for LazyLock<T, I> {
@@ -66,6 +71,17 @@ impl<T, I: FnOnce() -> T> Deref for LazyLock<T, I> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        self.get_blocking()
+        self.force()
+    }
+}
+impl<T: Debug, I> Debug for LazyLock<T, I> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let mut struct_ = f.debug_struct("LazyLock");
+        if self.once.is_complete() {
+            struct_.field("data", unsafe { (*self.data.get()).data.assume_init_ref() });
+        } else {
+            struct_.field("data", &"Uninitialized");
+        }
+        struct_.finish()
     }
 }
