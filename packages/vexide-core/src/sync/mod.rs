ed1305e3c7f19e1e62cf0121fbd6434d7cd33c8b
Gavin-Niederman <gavinniederman@gmail.com>
2024-05-02 11:26:25 -0700

    chore: fmt

--- packages/vexide-core/src/sync/mod.rs
+++ packages/vexide-core/src/sync/mod.rs
@@ -4,14 +4,14 @@
 
 mod barrier;
 mod condvar;
+mod lazy;
 mod mutex;
 mod once;
 mod rwlock;
-mod lazy;
 
 pub use barrier::{Barrier, BarrierWaitFuture};
 pub use condvar::{Condvar, CondvarWaitFuture};
+pub use lazy::LazyLock;
 pub use mutex::{Mutex, MutexGuard, MutexLockFuture, RawMutex};
 pub use once::{Once, OnceLock};
 pub use rwlock::{RwLock, RwLockReadFuture, RwLockReadGuard, RwLockWriteFuture, RwLockWriteGuard};
-pub use lazy::LazyLock;
