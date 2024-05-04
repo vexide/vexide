20a3f96bc967b8646de2883a952bedbd61c779fe
Gavin-Niederman <gavinniederman@gmail.com>
2024-05-03 22:56:24 -0700

    docs: make booting example much more useful

--- packages/vexide-startup/examples/booting.rs
+++ packages/vexide-startup/examples/booting.rs
@@ -1,23 +1,34 @@
+//! Minimal example of setting up program booting without the `#[vexide::main]` attribute macro.
+
 #![no_main]
 #![no_std]
 
 extern crate alloc;
 use alloc::boxed::Box;
-use core::time::Duration;
 
-use vexide_async::sleep;
 use vexide_core::println;
 
-#[vexide::main]
-async fn main(_peripherals: Peripherals) {
+#[no_mangle]
+extern "Rust" fn main() {
     unsafe {
         // Write something to the screen to test if the program is running
         let test_box = Box::new(100);
         vex_sdk::vexDisplayRectFill(0, 0, *test_box, 200);
         println!("Hello, world!");
-
-        loop {
-            sleep(Duration::from_millis(100)).await;
-        }
     }
+}
+
+#[no_mangle]
+#[link_section = ".boot"]
+unsafe extern "C" fn _entry() {
+    unsafe { vexide_startup::program_entry() }
+}
+
+#[link_section = ".cold_magic"]
+#[used] // This is needed to prevent the linker from removing this object in release builds
+static COLD_HEADER: vexide_startup::ColdHeader = vexide_startup::ColdHeader::new(2, 0, 0);
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
