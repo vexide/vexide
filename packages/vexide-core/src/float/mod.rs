da0bb0e7e45c46dfa30731038283b31d70ee0ea3
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-05-30 22:49:26 -0500

    fix: target_arch condition attribute

--- packages/vexide-core/src/float/mod.rs
+++ packages/vexide-core/src/float/mod.rs
@@ -5,7 +5,7 @@
 
 //! Provides implementations for the `critical_section` crate on the V5 brain and in WASM environments.
 
-#[cfg(all(target_arch = "arm"))]
+#[cfg(target_arch = "arm")]
 mod vexos;
 
 #[cfg(target_arch = "wasm32")]
