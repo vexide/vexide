9bceae6350242aeca036832df6c33f31f8d54834
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-05-30 22:46:59 -0500

    fix: switch back to good libm, revert basic example

--- packages/vexide/examples/basic.rs
+++ packages/vexide/examples/basic.rs
@@ -1,19 +1,9 @@
 #![no_main]
 #![no_std]
 
-use vexide::{core::time::Instant, prelude::*};
+use vexide::prelude::*;
 
 #[vexide::main]
 async fn main(_peripherals: Peripherals) {
-    use core::hint::black_box;
-
-    println!("{}", f64::sqrt(55.0));
-    // unsafe {
-    //     let start = Instant::now();
-    //     black_box(for _ in 0..10_000_000 {
-    //         black_box(sqrt(black_box(35.0)));
-    //     });
-    //     let elapsed = start.elapsed();
-    //     println!("{:?}", elapsed);
-    // }
+    println!("Hello, world!");
 }
