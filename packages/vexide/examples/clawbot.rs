4a00efd7e8f936c1adef771efc124aa116bc4ab8
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-06-04 23:40:22 -0500

    fix: clawbot example

--- packages/vexide/examples/clawbot.rs
+++ packages/vexide/examples/clawbot.rs
@@ -7,12 +7,10 @@
 
 #![no_main]
 #![no_std]
-#![feature(error_in_core)]
 
 extern crate alloc;
 
-use alloc::boxed::Box;
-use core::{error::Error, time::Duration};
+use core::time::Duration;
 
 use vexide::prelude::*;
 
@@ -29,6 +27,7 @@ struct ClawBot {
 
 impl Competition for ClawBot {
     async fn autonomous(&mut self) {
+        // Basic example autonomous that moves the drivetrain 10 revolutions forwards.
         self.left_motor
             .set_target(MotorControl::Position(
                 Position::from_revolutions(10.0),
@@ -57,10 +56,26 @@ impl Competition for ClawBot {
             let limit_switch_pressed = self.arm_limit_switch.is_high().unwrap_or_default();
 
             // Controller Buttons
-            let r1_pressed = self.controller.right_trigger_1.is_pressed().unwrap_or_default();
-            let r2_pressed = self.controller.right_trigger_2.is_pressed().unwrap_or_default();
-            let l1_pressed = self.controller.left_trigger_1.is_pressed().unwrap_or_default();
-            let l2_pressed = self.controller.left_trigger_2.is_pressed().unwrap_or_default();
+            let r1_pressed = self
+                .controller
+                .right_trigger_1
+                .is_pressed()
+                .unwrap_or_default();
+            let r2_pressed = self
+                .controller
+                .right_trigger_2
+                .is_pressed()
+                .unwrap_or_default();
+            let l1_pressed = self
+                .controller
+                .left_trigger_1
+                .is_pressed()
+                .unwrap_or_default();
+            let l2_pressed = self
+                .controller
+                .left_trigger_2
+                .is_pressed()
+                .unwrap_or_default();
 
             // Simple arcade drive
             let forward = self.controller.left_stick.y().unwrap_or_default() as f64;
@@ -69,7 +84,7 @@ impl Competition for ClawBot {
             let mut right_voltage = (forward - turn) * Motor::MAX_VOLTAGE;
 
             // If we are pressing the bumpers, don't allow the motors to go in reverse
-            if left_bumper || right_bumper {
+            if left_bumper_pressed || right_bumper_pressed {
                 left = left.max(0.0);
                 right = right.max(0.0);
             }
@@ -79,16 +94,16 @@ impl Competition for ClawBot {
             self.right_motor.set_voltage(right_voltage).ok();
 
             // Arm control using the R1 and R2 buttons on the controller.
-            if r1 {
+            if r1_pressed {
                 self.arm.set_voltage(12.0).ok();
-            } else if r2 {
+            } else if r2_pressed {
                 self.arm.set_voltage(-12.0).ok();
             } else {
                 self.arm.brake(BrakeMode::Hold);
             }
 
             // Claw control using the L1 and L2 buttons on the controller.
-            if l1 {
+            if l1_pressed {
                 self.claw.set_voltage(12.0).ok();
             } else if l2_pressed && !limit_switch_pressed {
                 self.claw.set_voltage(-12.0).ok();
@@ -106,6 +121,7 @@ impl Competition for ClawBot {
 
 #[vexide::main]
 async fn main(peripherals: Peripherals) {
+    // Configuring devices and handing off control to the [`Competition`] API.
     ClawBot {
         left_motor: Motor::new(peripherals.port_1, Gearset::Green, Direction::Forward),
         right_motor: Motor::new(peripherals.port_10, Gearset::Green, Direction::Reverse),
