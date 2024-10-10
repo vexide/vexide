1a767ea7a4537a434a458571c7078dc6413b3103
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-10-09 22:35:10 -0500

    docs: update battery docs

--- packages/vexide-devices/src/battery.rs
+++ packages/vexide-devices/src/battery.rs
@@ -4,26 +4,22 @@ use vex_sdk::{
     vexBatteryCapacityGet, vexBatteryCurrentGet, vexBatteryTemperatureGet, vexBatteryVoltageGet,
 };
 
-/// Get the robot's battery capacity.
-/// TODO: Determine units
+/// Get the robot's current battery capacity as a percentage out of 100.
 pub fn capacity() -> f64 {
     unsafe { vexBatteryCapacityGet() }
 }
 
-/// Get the current temperature of the robot's battery.
-/// TODO: Determine units
+/// Get the current temperature of the robot's battery in degrees celsius.
 pub fn temperature() -> f64 {
     unsafe { vexBatteryTemperatureGet() }
 }
 
 /// Get the electric current of the robot's battery.
-/// TODO: Determine units
 pub fn current() -> i32 {
     unsafe { vexBatteryCurrentGet() }
 }
 
 /// Get the robot's battery voltage.
-/// TODO: Determine units
 pub fn voltage() -> i32 {
     unsafe { vexBatteryVoltageGet() }
 }
