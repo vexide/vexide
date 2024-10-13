49cb18d65791676fa13a3efbcab34de77b7968fb
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-10-13 13:20:45 -0500

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
           buildInputs = [
             cargo-v5'
             pkgs.cargo-binutils
-            (pkgs.rust-bin.nightly."2024-02-07".default.override {
+            (pkgs.rust-bin.nightly.default.override {
               extensions = [ "rust-analyzer" "rust-src" "clippy" "llvm-tools" ];
             })
           ];
