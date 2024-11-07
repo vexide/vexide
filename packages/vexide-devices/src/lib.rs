bd445f4acf5f09d6390e8651fea701ab21e9b280
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-11-06 21:11:24 -0600

    docs: remove misleading "Features" section, replace with peripherals explanation

--- packages/vexide-devices/src/lib.rs
+++ packages/vexide-devices/src/lib.rs
@@ -2,21 +2,39 @@
 //!
 //! # Overview
 //!
-//! This crate provides APIs for interfacing with VEX hardware.
+//! This crate provides APIs for interfacing with VEX hardware and peripherals.
 //!
 //! The V5 Brain features 21 RJ9 serial ports (known as "Smart Ports") for communicating
 //! with newer V5 devices, as well as six three-wire ports with analog-to-digital conversion
 //! capability for compatibility with legacy Cortex devices. The Brain also has a screen,
 //! battery, and usually a controller for reading user input.
 //!
-//! # Features
+//! Hardware access begins at the [`Peripherals`](crate::peripherals::Peripherals) API, where
+//! singleton access to the brain's I/O and peripherals can be obtained:
 //!
-//! - [`peripherals`]: Singleton-style peripheral access.
-//! - [`smart`]: Smart Ports and devices.
-//! - [`adi`]: Three-wire ports and devices.
-//! - [`battery`]: Battery API
-//! - [`display`]: Brain Display API
-//! - [`controller`]: Controller API
+//! ```
+//! let peripherals = Peripherals::take().unwrap();
+//!
+//! // Pull out port 1 of peripherals. This is a `SmartPort` and can be used to construct any
+//! // device on port 1 of the Brain that we want to control.
+//! let port_1 = peripherals.port_1;
+//! ```
+//!
+//! If you are on vexide, [`Peripherals`] is already given to you through your `main` function:
+//!
+//! ```
+//! #![no_std]
+//! #![no_main]
+//!
+//! use vexide::prelude::*;
+//!
+//! #[vexide::main]
+//! async fn main(peripherals: Peripherals) {
+//!     println!("o.o what's this? {:?}", peripherals);
+//! }
+//! ```
+//!
+//! For more information on peripheral access, see the [`peripherals`] module.
 
 #![no_std]
 
