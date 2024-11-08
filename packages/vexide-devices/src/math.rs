0e76e88d3ebf30d8794dc406fd1cd91b0e26f267
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-11-08 01:04:13 -0600

    chore: fmt

--- packages/vexide-devices/src/math.rs
+++ packages/vexide-devices/src/math.rs
@@ -3,4 +3,4 @@
 //! This module re-exports several math-related types from the [`mint`] crate
 //! for usage in vexide's device APIs.
 
-pub use mint::{EulerAngles, Quaternion, Vector3, Point2};
+pub use mint::{EulerAngles, Point2, Quaternion, Vector3};
