a416c04aa59d86b128b2a5416868763f3f44a04d
zabackary <137591653+zabackary@users.noreply.github.com>
2024-11-16 23:15:11 +0900

    chore: update panic_hooks example

--- packages/vexide/examples/panic_hooks.rs
+++ packages/vexide/examples/panic_hooks.rs
@@ -3,18 +3,32 @@
 
 use core::time::Duration;
 
-use vexide::prelude::*;
-use vexide_devices::controller::ControllerId;
-use vexide_panic::set_panic_hook;
+use vexide::{
+    devices::{controller::ControllerId, geometry::Point2, screen::Rect},
+    prelude::*,
+};
 
 #[vexide::main]
 async fn main(_peripherals: Peripherals) {
     println!("Hello, world!");
 
-    set_panic_hook(|_| {
+    vexide::panic::set_hook(|info| {
+        // Show the panic message on the primary controller
         let mut controller_primary = unsafe { Controller::new(ControllerId::Primary) };
-
         let _ = controller_primary.screen.set_text("Panic!", 0, 0);
+
+        // Fill the screen with red
+        let mut screen = unsafe { Screen::new() };
+        screen.fill(
+            &Rect::from_dimensions(
+                Point2 { x: 0, y: 0 },
+                Screen::HORIZONTAL_RESOLUTION as u16,
+                Screen::VERTICAL_RESOLUTION as u16,
+            ),
+            0xff0000 as u32,
+        );
+
+        vexide::panic::default_panic_hook(&info);
     });
 
     sleep(Duration::from_millis(1000)).await;
