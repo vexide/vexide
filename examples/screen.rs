bba0fa798647e2cab63253a5ac9ff46ac557f7f0
doinkythederp <doinkythederp@icloud.com>
2024-11-23 19:50:27 -0800

    feat: add advanced font config

--- examples/screen.rs
+++ examples/screen.rs
@@ -4,20 +4,19 @@
 use core::{fmt::Write, time::Duration};
 
 use vexide::{
-    devices::display::{Rect, Text, TextSize},
+    devices::display::{Font, FontFamily, FontSize, Rect, Text},
     prelude::*,
 };
 
 #[vexide::main]
 async fn main(peripherals: Peripherals) {
-    // We can get the screen directly from peripherals becuase it is always connected to the Brain.
+    // We can get the screen directly from peripherals because it is always connected to the Brain.
     let mut display = peripherals.display;
 
-    // Print a message to the screen
+    // Print a message to the screen. This will be displayed in the top left corner.
     write!(display, "Hello, world!").unwrap();
 
-    // Create a rectangle to be drawn on the screen.
-    let rect = Rect::new([20, 20], [100, 100]);
+    let rect = Rect::new([20, 20], [120, 120]);
 
     // Fill in the entire rectangle with white.
     display.fill(&rect, Rgb::new(255, 255, 255));
@@ -25,10 +24,19 @@ async fn main(peripherals: Peripherals) {
     // This will appear on top of the white fill because it is called later.
     display.stroke(&rect, Rgb::new(255, 0, 255));
 
-    // Create a piece of text to draw on the screen at a specific position.
-    let text = Text::new("Nice to see you!", TextSize::Medium, [80, 80]);
-    // Fill in the text with cyan.
+    let text = Text::new("Nice to see you!", Font::default(), [80, 40]);
+
+    // Draw the text on the display in cyan.
     display.fill(&text, Rgb::new(0, 255, 255));
+
+    // You can use varying text sizes and fonts.
+
+    let text = Text::new(
+        "This is vexide.",
+        Font::new(FontSize::new(1, 1), FontFamily::Proportional),
+        [21, 21],
+    );
+    display.fill(&text, Rgb::new(255, 255, 255));
 
     loop {
         sleep(Duration::from_secs(1)).await;
