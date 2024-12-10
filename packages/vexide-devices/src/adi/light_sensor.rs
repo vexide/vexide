0becebfadd128fc3caff315cef7647897744fffc
zabackary <137591653+zabackary@users.noreply.github.com>
2024-12-10 21:52:50 +0900

    chore: add examples for AdiLightSensor

--- packages/vexide-devices/src/adi/light_sensor.rs
+++ packages/vexide-devices/src/adi/light_sensor.rs
@@ -13,7 +13,7 @@
 //! # Effective Range
 //!
 //! Effective range is dependent on both the intensity of the source and the surrounding
-//! enviornment. Darker ambient surroundings with a brighter source will result in a
+//! environment. Darker ambient surroundings with a brighter source will result in a
 //! greater effective range.
 //!
 //! That being said, the sensor generally has a usable range of up to 6 feet, meaning it
@@ -32,7 +32,22 @@ pub struct AdiLightSensor {
 }
 
 impl AdiLightSensor {
-    /// Create a light sensor from an ADI port.
+    /// Create a light sensor on the given [`AdiPort`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use vexide::prelude::*;
+    ///
+    /// #[vexide::main]
+    /// async fn main(peripherals: Peripherals) {
+    ///     // Create a new light sensor on port 1
+    ///     let sensor = AdiLightSensor::new(peripherals.port_1);
+    ///
+    ///     // Get the brightness value
+    ///     println!("Brightness value: {:?}", sensor.brightness());
+    /// }
+    /// ```
     #[must_use]
     pub fn new(port: AdiPort) -> Self {
         port.configure(AdiDeviceType::LightSensor);
@@ -43,13 +58,28 @@ impl AdiLightSensor {
     /// Returns the brightness factor measured by the sensor. Higher numbers mean
     /// a brighter light source.
     ///
-    /// This is returned as a value ranging from [0.0, 1.0].
+    /// This is returned as a value in the interval [0.0, 1.0].
     ///
     /// # Errors
     ///
     /// - A [`PortError::Disconnected`] error is returned if an ADI expander device was required but not connected.
     /// - A [`PortError::IncorrectDevice`] error is returned if an ADI expander device was required but
     ///   something else was connected.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use vexide::prelude::*;
+    ///
+    /// #[vexide::main]
+    /// async fn main(peripherals: Peripherals) {
+    ///     // Create a new light sensor on port 1
+    ///     let sensor = AdiLightSensor::new(peripherals.port_1);
+    ///
+    ///     // Get the brightness value
+    ///     println!("Brightness value: {:?}", sensor.brightness());
+    /// }
+    /// ```
     pub fn brightness(&self) -> Result<f64, PortError> {
         Ok(f64::from(analog::ADC_MAX_VALUE - self.raw_brightness()?)
             / f64::from(analog::ADC_MAX_VALUE))
@@ -57,8 +87,8 @@ impl AdiLightSensor {
 
     /// Returns the 12-bit brightness reading of the sensor.
     ///
-    /// This is a raw 12-bit value from [0, 4095] representing the voltage level from
-    /// 0-%V measured by the V5 Brain's ADC.
+    /// This is a raw 12-bit value in the interval [0, 4095] representing the voltage level from
+    /// 0-5V measured by the V5 Brain's ADC.
     ///
     /// A low number (less voltage) represents a **brighter** light source.
     ///
@@ -67,6 +97,21 @@ impl AdiLightSensor {
     /// - A [`PortError::Disconnected`] error is returned if an ADI expander device was required but not connected.
     /// - A [`PortError::IncorrectDevice`] error is returned if an ADI expander device was required but
     ///   something else was connected.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use vexide::prelude::*;
+    ///
+    /// #[vexide::main]
+    /// async fn main(peripherals: Peripherals) {
+    ///     // Create a new light sensor on port 1
+    ///     let sensor = AdiLightSensor::new(peripherals.port_1);
+    ///
+    ///     // Get the brightness value
+    ///     println!("Raw 12-bit brightness value: {:?}", sensor.raw_brightness());
+    /// }
+    /// ```
     pub fn raw_brightness(&self) -> Result<u16, PortError> {
         self.port.validate_expander()?;
 
