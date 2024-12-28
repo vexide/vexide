a302543833e103a67a2f2011cf7d4f8e3a9532f4
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-12-27 23:56:35 -0600

    test: patch uploading MVP

--- packages/vexide-startup/src/lib.rs
+++ packages/vexide-startup/src/lib.rs
@@ -13,10 +13,15 @@
 #![no_std]
 #![allow(clippy::needless_doctest_main)]
 
+extern crate alloc;
+
 use banner::themes::BannerTheme;
 use bitflags::bitflags;
+use varint_slop::VarIntReader;
+use vexide_core::io::{Cursor, Read, Seek, SeekFrom};
 
 pub mod banner;
+pub(crate) mod varint_slop;
 
 /// Identifies the type of binary to VEXos.
 #[repr(u32)]
@@ -83,6 +88,9 @@ impl CodeSignature {
 }
 
 unsafe extern "C" {
+    static mut __heap_start: u8;
+    static mut __heap_end: u8;
+
     // These symbols don't have real types, so this is a little bit of a hack.
     static mut __bss_start: u32;
     static mut __bss_end: u32;
@@ -104,6 +112,17 @@ _boot:
     b _start             @ Jump to the Rust entrypoint.
 "#
 );
+
+// TODO: rewrite in assembly to prevent this function from possibly patching itself due to
+// differences between debug/release codegen and rustc updates.
+#[unsafe(link_section = ".overwriter")]
+#[inline(never)]
+unsafe fn overwrite_with_new(new: &[u8]) -> ! {
+    unsafe {
+        core::ptr::copy_nonoverlapping(new.as_ptr(), 0x0380_0000 as _, new.len());
+        core::arch::asm!("b _boot", options(noreturn));
+    }
+}
 
 /// Zeroes the `.bss` section
 ///
@@ -136,6 +155,13 @@ where
     }
 }
 
+#[derive(Debug)]
+enum PatcherState {
+    Initial,
+    Add(usize),
+    Copy(usize),
+}
+
 /// Startup Routine
 ///
 /// - Sets up the heap allocator if necessary.
@@ -154,12 +180,132 @@ pub unsafe fn startup<const BANNER: bool>(theme: BannerTheme) {
             core::ptr::addr_of_mut!(__bss_start),
             core::ptr::addr_of_mut!(__bss_end),
         );
-
-        // Initialize the heap allocator
-        vexide_core::allocator::init_heap();
     }
 
-    // Print the banner
+    'patch: {
+        const PATCH_MAGIC: u32 = 0xB1DF;
+        const PATCH_VERSION: u32 = 0x1000;
+        const USER_MEMORY_START: u32 = 0x0380_0000;
+        const PATCH_MEMORY_START: u32 = 0x0780_0000;
+
+        let link_addr = unsafe { vex_sdk::vexSystemLinkAddrGet() };
+
+        // This means we might potentially have a patch that needs to be applied.
+        if link_addr == USER_MEMORY_START {
+            // Pointer to the linked file in memory.
+            let patch_ptr = PATCH_MEMORY_START as *mut u32;
+
+            unsafe {
+                // We first need to validate that the linked file is indeed a patch. The first 32 bits
+                // (starting at link_addr+0) should always be 0xB1DF, and the 32 bits after should contain
+                // a version constant that matches ours. If either of these checks fail, then we boot normally.
+                if patch_ptr.read() != PATCH_MAGIC || patch_ptr.offset(1).read() != PATCH_VERSION {
+                    // TODO: reclaim as heap space.
+                    break 'patch;
+                }
+
+                // Overwrite patch magic so we don't re-apply the patch next time.
+                patch_ptr.write(0xB2DF);
+
+                // Next few bytes contain metadata about how large our current binary is, as well as the length of
+                // the patch itself. We need this for the next step.
+                let patch_len = patch_ptr.offset(2).read();
+                let old_binary_len = patch_ptr.offset(3).read();
+                let new_binary_len = patch_ptr.offset(4).read();
+
+                // We have to ensure that the heap does not overlap the memory space from the new binary.
+                vexide_core::allocator::claim(
+                    (USER_MEMORY_START + new_binary_len) as *mut u8,
+                    &raw mut __heap_end,
+                );
+
+                // Slice representing our patch contents.
+                let mut patch = core::slice::from_raw_parts(
+                    patch_ptr.offset(5).cast(),
+                    patch_len as usize - (size_of::<u32>() * 5),
+                );
+
+                // Slice of the executable portion of the currently running program (this one!)
+                let mut old = Cursor::new(core::slice::from_raw_parts_mut(
+                    USER_MEMORY_START as *mut u8,
+                    old_binary_len as usize,
+                ));
+
+                // `bidiff` does not patch in-place, meaning we need a copy of our currently running binary on the heap
+                // that we will apply our patch to using our actively running binary as a reference point for the "old" bits.
+                // After that, `apply_patch` will handle safely overwriting user code with our "new" version on the heap.
+                let mut new_vec = alloc::vec![0; new_binary_len as usize];
+                let mut new: &mut [u8] = new_vec.as_mut_slice();
+
+                // Apply the patch onto `new`, using `old` as a reference.
+                //
+                // This is basically a port of <https://github.com/divvun/bidiff/blob/main/crates/bipatch/src/lib.rs>
+
+                let mut buf = alloc::vec![0u8; 4096];
+
+                let mut state = PatcherState::Initial;
+
+                while !new.is_empty() {
+                    let processed = match state {
+                        PatcherState::Initial => {
+                            state = PatcherState::Add(patch.read_varint().unwrap());
+                            0
+                        }
+                        PatcherState::Add(add_len) => {
+                            let n = add_len.min(new.len()).min(buf.len());
+
+                            let out = &mut new[..n];
+                            old.read_exact(out).unwrap();
+
+                            let dif = &mut buf[..n];
+                            patch.read_exact(dif).unwrap();
+
+                            for i in 0..n {
+                                out[i] = out[i].wrapping_add(dif[i]);
+                            }
+
+                            state = if add_len == n {
+                                let copy_len: usize = patch.read_varint().unwrap();
+                                PatcherState::Copy(copy_len)
+                            } else {
+                                PatcherState::Add(add_len - n)
+                            };
+
+                            n
+                        }
+                        PatcherState::Copy(copy_len) => {
+                            let n = copy_len.min(new.len());
+
+                            let out = &mut new[..n];
+                            patch.read_exact(out).unwrap();
+
+                            state = if copy_len == n {
+                                let seek: i64 = patch.read_varint().unwrap();
+                                old.seek(SeekFrom::Current(seek)).unwrap();
+
+                                PatcherState::Initial
+                            } else {
+                                PatcherState::Copy(copy_len - n)
+                            };
+
+                            n
+                        }
+                    };
+
+                    new = &mut new[processed..];
+                }
+
+                overwrite_with_new(&new_vec);
+            }
+        }
+    }
+
+    // Initialize the heap allocator using normal bounds
+    unsafe {
+        vexide_core::allocator::claim(&raw mut __heap_start, &raw mut __heap_end);
+    }
+
+    // Print the bannerx
     if BANNER {
         banner::print(theme);
     }
