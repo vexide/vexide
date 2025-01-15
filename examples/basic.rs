c2f9a4bd25d291f0e27284220894d2e684b4c38b
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-01-14 18:09:01 -0600

    chore: remove test code

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,15 +1,9 @@
 #![no_main]
 #![no_std]
 
-use core::time::Duration;
-
 use vexide::prelude::*;
 
 #[vexide::main]
 async fn main(_peripherals: Peripherals) {
-    println!("helo :33");
-
-    loop {
-        sleep(Duration::from_millis(500)).await;
-    }
+    println!("Hello, world!");
 }
