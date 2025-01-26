63ca5b52702fb351434db433a910f8454e4aa883
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-01-25 20:13:07 -0600

    refactor: naming consistency with `vex-v5-serial`

--- packages/vexide-core/src/os.rs
+++ packages/vexide-core/src/os.rs
@@ -6,8 +6,8 @@ use core::fmt;
 
 use vex_sdk::vexSystemVersion;
 
-/// A VexOS version
-#[derive(Clone, Copy, Debug)]
+/// A VEXos version
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub struct Version {
     /// The major version
     pub major: u8,
@@ -15,16 +15,16 @@ pub struct Version {
     pub minor: u8,
     /// The build version
     pub build: u8,
-    /// The patch version
-    pub patch: u8,
+    /// The beta version
+    pub beta: u8,
 }
 
 impl fmt::Display for Version {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "{}.{}.{}-r{}",
-            self.major, self.minor, self.patch, self.patch
+            "{}.{}.{}-b{}",
+            self.major, self.minor, self.build, self.beta
         )
     }
 }
@@ -37,6 +37,6 @@ pub fn system_version() -> Version {
         major: version_bytes[0],
         minor: version_bytes[1],
         build: version_bytes[2],
-        patch: version_bytes[3],
+        beta: version_bytes[3],
     }
 }
