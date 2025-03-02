86267f7645e24879e8785e9ef3679764b0310b28
Lewis <mcclelll@icloud.com>
2025-03-01 22:12:42 -0800

    fix: cfg() allocator re-export

--- packages/vexide/src/lib.rs
+++ packages/vexide/src/lib.rs
@@ -65,7 +65,10 @@ pub mod time {
 
 #[doc(inline)]
 #[cfg(feature = "core")]
-pub use vexide_core::{allocator, backtrace, competition, float, fs, io, os, path, program, sync};
+pub use vexide_core::{backtrace, competition, float, fs, io, os, path, program, sync};
+#[doc(inline)]
+#[cfg(feature = "allocator")]
+pub use vexide_core::allocator;
 #[doc(inline)]
 #[cfg(feature = "devices")]
 pub use vexide_devices as devices;
