700229aaf86a78b2c4c4882103c20344ef2a3d28
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-04-26 23:50:04 -0500

    fix: use `min` rather than `max`

--- packages/vexide-devices/src/adi/addrled.rs
+++ packages/vexide-devices/src/adi/addrled.rs
@@ -62,7 +62,7 @@ impl<const N: usize> AdiAddrLed<N> {
     }
 
     fn update(&mut self, buf: &[u32], offset: usize) {
-        let buf = &buf[offset..buf.len().max(N)];
+        let buf = &buf[offset..buf.len().min(N)];
 
         unsafe {
             vexDeviceAdiAddrLedSet(
