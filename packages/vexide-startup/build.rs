c0abe5b7378d96740ad76876d4b10aa0cb6fba4b
Ayaan B <ayaanbarbhuiya098@gmail.com>
2025-08-25 21:54:00 +0000

    style: fix code formatting

--- packages/vexide-startup/build.rs
+++ packages/vexide-startup/build.rs
@@ -4,13 +4,18 @@ use std::env;
 
 fn main() {
     println!("cargo:rerun-if-env-changed=CARGO_CFG_VEXIDE_UPLOAD_STRATEGY");
-    println!(r#"cargo::rustc-check-cfg=cfg(vexide_upload_strategy, values("monolith", "differential"))"#);
+    println!(
+        r#"cargo::rustc-check-cfg=cfg(vexide_upload_strategy, values("monolith", "differential"))"#
+    );
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
-    let upload_strategy = env::var("CARGO_CFG_VEXIDE_UPLOAD_STRATEGY").unwrap_or("differential".into());
+    let upload_strategy =
+        env::var("CARGO_CFG_VEXIDE_UPLOAD_STRATEGY").unwrap_or("differential".into());
     match upload_strategy.as_str() {
-        "differential" => {}, // Link script needs no overriding for differential uploads
+        "differential" => {} // Link script needs no overriding for differential uploads
         "monolith" => println!("cargo:rustc-link-arg=--defsym=__patcher_section_length=0"),
-        value => panic!(r#"Unknown value "{value}" for vexide_upload_strategy! Valid values are "differential" and "monolith""#)
+        value => panic!(
+            r#"Unknown value "{value}" for vexide_upload_strategy! Valid values are "differential" and "monolith""#
+        ),
     };
 
     println!("cargo:rustc-link-search=native={manifest_dir}/link");
