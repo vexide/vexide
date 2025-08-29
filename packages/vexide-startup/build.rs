fc1b02314f75c28fa2a0c48f7148786c06c95975
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-08-29 13:35:52 -0500

    refactor: add support for `vex-sdk-build` in `vexide-startup`

--- packages/vexide-startup/build.rs
+++ packages/vexide-startup/build.rs
@@ -3,4 +3,7 @@
 fn main() {
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     println!("cargo:rustc-link-search=native={manifest_dir}/link");
+
+    #[cfg(not(feature = "vex-sdk-build"))]
+    vex_sdk_build::link_sdk("V5_20240802_15_00_00");
 }
