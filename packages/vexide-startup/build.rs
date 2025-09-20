6d73c2e4825107bde8d49f1fd9cd3daf256c06f2
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-09-20 16:36:05 -0500

    fix: simplify vex-sdk-download conditional compilation

--- packages/vexide-startup/build.rs
+++ packages/vexide-startup/build.rs
@@ -4,6 +4,9 @@ fn main() {
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     println!("cargo:rustc-link-search=native={manifest_dir}/link");
 
-    #[cfg(all(feature = "vex-sdk-download", not(feature = "vex-sdk-mock")))]
+    #[cfg(all(
+        target_os = "vexos",
+        feature = "vex-sdk-download"
+    ))]
     vex_sdk_download::link_sdk("V5_20240802_15_00_00");
 }
