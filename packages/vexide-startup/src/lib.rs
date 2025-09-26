a7c0e645bfb950ca3201f5ada0bbd0230a8f208d
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-09-26 11:02:23 -0500

    refactor: simplify `backtraces`/`panic-backtraces` feature

--- packages/vexide-startup/src/lib.rs
+++ packages/vexide-startup/src/lib.rs
@@ -144,7 +144,7 @@ unsafe extern "C" fn _vexide_boot() {
 ///   and resets the program if necessary.
 /// - Registers a custom [panic hook] to allow panic messages to be drawn to the screen and
 ///   backtraces to be collected. This can be enabled/disabled using the `panic-hook` and
-///   `panic-backtraces` features.
+///   `backtraces` features.
 ///
 /// [differential upload patches]: https://vexide.dev/docs/building-uploading/#uploading-strategies
 /// [panic hook]: https://doc.rust-lang.org/std/panic/fn.set_hook.html
