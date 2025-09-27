f20d35dd71f1c896f62846d30d17589e10bcc0cd
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-09-27 13:16:10 -0500

    fix: conditional compilation for vex-sdk-vexcode
    chore: fmt

--- packages/vexide-startup/build.rs
+++ packages/vexide-startup/build.rs
@@ -4,6 +4,11 @@ fn main() {
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     println!("cargo:rustc-link-search=native={manifest_dir}/link");
 
-    #[cfg(all(target_os = "vexos", feature = "vex-sdk-vexcode"))]
-    vex_sdk_vexcode::link_sdk("V5_20240802_15_00_00");
+    #[cfg(feature = "vex-sdk-vexcode")]
+    {
+        let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+        if target_os == "vexos" {
+            vex_sdk_vexcode::link_sdk("V5_20240802_15_00_00");
+        }
+    }
 }
