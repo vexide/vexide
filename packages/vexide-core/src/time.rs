43ddf560580a11bdb804f811a12c1e4670adb695
Tropical <42101043+tropicaaal@users.noreply.github.com>
2025-10-13 15:12:10 -0500

    docs: improved documentation comments, lint

--- packages/vexide-core/src/time.rs
+++ packages/vexide-core/src/time.rs
@@ -2,15 +2,27 @@
 
 use core::time::Duration;
 
-use vex_sdk::{vexSystemHighResTimeGet, vexSystemPowerupTimeGet, vexSystemTimeGet};
+use vex_sdk::{vexSystemHighResTimeGet, vexSystemPowerupTimeGet};
 
-/// Returns the duration that the brain has been powered on for.
+/// Returns the duration that the brain has been powered on.
+///
+/// # Precision
+///
+/// The returned [`Duration`] has a precision of 1 microsecond.
 #[must_use]
 pub fn system_uptime() -> Duration {
     Duration::from_micros(unsafe { vexSystemPowerupTimeGet() })
 }
 
-/// Returns the duration that the user processor has been running for.
+/// Returns the duration that the brain's user processor has been
+/// running.
+///
+/// This is effectively the amount of time that the current user
+/// program has been running.
+///
+/// # Precision
+///
+/// The returned [`Duration`] has a precision of 1 microsecond.
 #[must_use]
 pub fn user_uptime() -> Duration {
     Duration::from_micros(unsafe { vexSystemHighResTimeGet() })
