f54401d96386fb447ee612b0f16551505c0e8f44
ion098 <146852218+ion098@users.noreply.github.com>
2025-10-30 11:02:32 -0700

    style: rustfmt, take ∞ + 2

--- packages/vexide-devices/src/display.rs
+++ packages/vexide-devices/src/display.rs
@@ -590,7 +590,7 @@ impl<'a> Text<'a> {
     ) -> Self {
         Self {
             text: CowTextStr::Owned(
-                CString::new(text).expect("Null character in non-terminating position")
+                CString::new(text).expect("Null character in non-terminating position"),
             ),
             position: position.into(),
             font,
